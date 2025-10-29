7dd3266380fce39870507ce35e0f437b8d35f095
mhfabripresent <matheusfabri335@gmail.com>
2025-10-28 22:39:47 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 function clickhere() {
     window.location.href = "https://mhfabripresent.github.io/lovelist";
 }
+
+function spotify() {
+    window.location.href = "https://open.spotify.com/playlist/6H9xqbOiu9n0ivBorXEP9C?si=19457389ecce495e";
+}
